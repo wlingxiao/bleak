fbfc118c70a6cad7524b4e2e2627fa9c355683ee
wlingxiao <wlingxiao@outlook.com>
2018-07-01 08:40:16 +0800

    add more logger api

--- core/src/main/scala/goa/logging/Logging.scala
+++ core/src/main/scala/goa/logging/Logging.scala
@@ -8,10 +8,24 @@ private[goa] trait Logging {
 
 private[goa] abstract class Logger {
 
+  def trace(msg: => String): Unit
+
+  def trace(msg: => String, t: Throwable): Unit
+
   def debug(msg: => String): Unit
 
-  def error(msg: => String, t: => Throwable): Unit
+  def debug(msg: => String, t: Throwable): Unit
 
   def info(msg: => String): Unit
+
+  def info(msg: => String, t: Throwable): Unit
+
+  def warn(msg: => String): Unit
+
+  def warn(msg: => String, t: Throwable): Unit
+
+  def error(msg: String): Unit
+
+  def error(msg: => String, t: => Throwable): Unit
 
 }
--- core/src/main/scala/goa/logging/Slf4JLogger.scala
+++ core/src/main/scala/goa/logging/Slf4JLogger.scala
@@ -2,23 +2,63 @@ package goa.logging
 
 private[goa] class Slf4JLogger(underlying: org.slf4j.Logger) extends Logger {
 
-  override def debug(msg: => String): Unit = {
-    underlying.isTraceEnabled()
+  override def trace(msg: => String): Unit = {
+    if (underlying.isTraceEnabled) {
+      underlying.trace(msg)
+    }
+  }
 
+  override def trace(msg: => String, t: Throwable): Unit = {
+    if (underlying.isTraceEnabled) {
+      underlying.trace(msg, t)
+    }
+  }
+
+  override def debug(msg: => String): Unit = {
     if (underlying.isDebugEnabled) {
       underlying.debug(msg)
     }
   }
 
-  override def error(msg: => String, t: => Throwable): Unit = {
-    if (underlying.isErrorEnabled) {
-      underlying.error(msg, t)
+  override def debug(msg: => String, t: Throwable): Unit = {
+    if (underlying.isDebugEnabled) {
+      underlying.debug(msg, t)
     }
   }
 
   override def info(msg: => String): Unit = {
     if (underlying.isInfoEnabled) {
       underlying.info(msg)
+    }
+  }
+
+  override def info(msg: => String, t: Throwable): Unit = {
+    if (underlying.isInfoEnabled()) {
+      underlying.info(msg, t)
+    }
+  }
+
+  override def warn(msg: => String): Unit = {
+    if (underlying.isWarnEnabled) {
+      underlying.warn(msg)
+    }
+  }
+
+  override def warn(msg: => String, t: Throwable): Unit = {
+    if (underlying.isWarnEnabled) {
+      underlying.warn(msg, t)
+    }
+  }
+
+  override def error(msg: String): Unit = {
+    if (underlying.isErrorEnabled) {
+      underlying.error(msg)
+    }
+  }
+
+  override def error(msg: => String, t: => Throwable): Unit = {
+    if (underlying.isErrorEnabled) {
+      underlying.error(msg, t)
     }
   }
 
