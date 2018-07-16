dba1b06241db034ce213a3cc5b35c35f0e35835d
wlingxiao <wlingxiao@outlook.com>
2018-07-16 10:22:01 +0800

    add specification to server interface

--- core/src/main/scala/goa/channel/Server.scala
+++ core/src/main/scala/goa/channel/Server.scala
@@ -2,10 +2,19 @@ package goa.channel
 
 trait Server {
 
+  /**
+    * Start the server which is bound to the specified host and port
+    */
   def start(host: String, port: Int): Unit
 
+  /**
+    * Blocks current server
+    */
   def join(): Unit
 
+  /**
+    * Immediately shutdown the [[Server]] instance
+    */
   def stop(): Unit
 
 }
