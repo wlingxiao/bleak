70cce1cf026baea9a8bfa4e5d9265c54f0981434
wlingxiao <wlingxiao@outlook.com>
2018-08-05 19:06:27 +0800

    update swagger example

--- swagger/src/test/scala/example/UserController.scala
+++ swagger/src/test/scala/example/UserController.scala
@@ -7,20 +7,22 @@ import goa._
 import scala.annotation.meta.field
 import scala.beans.BeanProperty
 
-@ApiModel(description = "5464")
+@ApiModel(description = "user param")
 case class User(@BeanProperty @(ApiModelProperty@field)(value = "username") name: String,
                 @BeanProperty @(ApiModelProperty@field)(value = "age") age: Long)
+
+case class BadRequestResult(@BeanProperty @(ApiModelProperty@field)(value = "error message") msg: String)
 
 @Api(tags = Array("user"))
 @route("/users") class UserController {
 
   @ApiResponses(Array(
-    new ApiResponse(code = 200, message = "success", reference = "#/definitions/User"),
-    new ApiResponse(code = 400, message = "bad request")))
-  @ApiOperation(value = "filter users", produces = "application/json;utf-8", response = classOf[User])
+    new ApiResponse(code = 400, message = "bad request", response = classOf[BadRequestResult]),
+    new ApiResponse(code = 500, message = "internal server error", response = classOf[BadRequestResult])))
+  @ApiOperation(value = "filter users", produces = "application/json;utf-8")
   @get def filterUsers(@path id: Long): List[User] = {
     response.contentType = "application/json;utf-8"
-    List(User("admin", 10))
+    List(User("admin", 10)) // FIXME cannot parse scala generic type
   }
 
   @ApiOperation(value = "get user by user id", produces = "application/json;utf-8", response = classOf[User])
