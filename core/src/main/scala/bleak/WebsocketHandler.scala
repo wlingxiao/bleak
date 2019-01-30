3734443b99d61a6436a188c4d0cadde2a1a66d75
wlingxiao <wlingxiao@outlook.com>
2019-01-30 16:04:31 +0800

    refactor

--- core/src/main/scala/bleak/WebsocketHandler.scala
+++ core/src/main/scala/bleak/WebsocketHandler.scala
@@ -3,6 +3,8 @@ import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
 import io.netty.handler.codec.http.HttpRequest
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler.HandshakeComplete
 
+import scala.concurrent.Future
+
 class WebsocketHandler(app: Application, route: WebsocketRoute)
     extends SimpleChannelInboundHandler[HttpRequest] {
 
@@ -14,11 +16,25 @@ class WebsocketHandler(app: Application, route: WebsocketRoute)
     evt match {
       case complete: HandshakeComplete =>
         val request = new HandshakeCompleteRequest(ctx, complete, Some(route))
+        val response = Response()
         val executeService = new WebSocketExecuteService
-        val future = app.globalMiddleware(request, executeService)
+        val context = WebsocketContext(request, response, app = null)
+
+        val future = app.received(context, executeService)
         responseWriter.write(ctx, request, future)
       case _ =>
         super.userEventTriggered(ctx, evt)
     }
 
+}
+
+private class HandshakeCompleteRequest(
+    ctx: ChannelHandlerContext,
+    complete: HandshakeComplete,
+    route: Option[WebsocketRoute])
+    extends Request.Proxy { override def request: Request = ??? }
+
+private class WebSocketExecuteService extends Service[Context, Context] {
+  override def apply(request: Context): Future[Context] =
+    ???
 }
