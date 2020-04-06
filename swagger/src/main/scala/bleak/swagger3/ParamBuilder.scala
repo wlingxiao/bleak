928de0d4bb77d93aced7029558a6429976daa5d0
wlingxiao <wlingxiao@outlook.com>
2020-04-06 16:49:30 +0800

    add response for param builder

--- swagger/src/main/scala/bleak/swagger3/ParamBuilder.scala
+++ swagger/src/main/scala/bleak/swagger3/ParamBuilder.scala
@@ -1,7 +1,8 @@
 package bleak.swagger3
 
 import io.swagger.v3.oas.models.{OpenAPI, Operation}
-import io.swagger.v3.oas.models.media.{Content, MediaType}
-import io.swagger.v3.oas.models.parameters.{Parameter, RequestBody}
 
-class ParamBuilder(val api: OpenAPI, val op: Operation) extends ParamFactory with RequestBodyFactory
+class ParamBuilder(val api: OpenAPI, val op: Operation)
+    extends ParamFactory
+    with RequestBodyFactory
+    with ResponseFactory
